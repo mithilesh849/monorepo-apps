3ac42e19afa883ec7c323de9b706b0d4943af18a
mithilesh <mithilesh849@gmail.com>
2025-12-30 02:23:10 +0530

    workflow updates

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -49,7 +49,7 @@ app.use((req, res) => {
 });
 
 // Error handler
-app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+app.use((err: Error, req: express.Request, res: express.Response, _next: express.NextFunction) => {
   console.error('Error:', err);
   res.status(500).json({ error: 'Internal server error' });
 });
